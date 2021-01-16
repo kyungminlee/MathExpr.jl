ef276110863d33ce8c3081ba13e5aa795a9f34e6
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2021-01-16 15:30:30 -0500

    Remove assets from docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,6 @@ makedocs(;
     repo="https://github.com/kyungminlee/MathExpr.jl/blob/{commit}{path}#L{line}",
     sitename="MathExpr.jl",
     authors="Kyungmin Lee",
-    assets=String[],
 )
 
 deploydocs(;

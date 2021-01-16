6d1631761d23f6d4fe92648c24fb17d854171d6f
Documenter.jl <documenter@juliadocs.github.io>
2021-01-16 15:38:14 +0000

    build based on f4e986c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MathExpr.jl","page":"Home","title":"MathExpr.jl","text":"","category":"section"},{"location":"#Exmples","page":"Home","title":"Exmples","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"julia> using MathExpr\n\njulia> parseexpr(\"cos(0.75π)+i*sin(0.5*pi)\")\n -0.7071067811865475 + 1.0im","category":"page"}]
+}
